563b8147269e47e73fc501b372e739eb86eabf06
Funny-pixel765 <funnyrnd92@gmail.com>
2025-03-12 16:26:22 +0700

    menambahkan class kodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using mod4_103022330036;
+
+internal class Program
+{
+    private static void Main(string[] args)
+{
+    kodeProduk produk = new kodeProduk();
+    Console.WriteLine("Laptop " +produk.getKodeProduk("Laptop"));
+    Console.WriteLine("Smartphone " +produk.getKodeProduk("Smartphone"));
+    Console.WriteLine("Tablet " +produk.getKodeProduk("Tablet"));
+    Console.WriteLine("Headset " +produk.getKodeProduk("Headset"));
+    Console.WriteLine("Keyboard " +produk.getKodeProduk("Keyboard"));
+    Console.WriteLine("Mouse  "+produk.getKodeProduk("Mouse"));
+    Console.WriteLine("Printer "+ produk.getKodeProduk("Printer"));
+    Console.WriteLine("Monitor "+ produk.getKodeProduk("Monitor"));
+        Console.WriteLine("Smartwatch " + produk.getKodeProduk("Smartwatch"));
+        Console.WriteLine("Kamera " + produk.getKodeProduk("Kamera"));
+}
+}
---
+++ kodeProduk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod4_103022330036
+{
+    public class kodeProduk
+    {
+        private static Dictionary<string, string> Produk = new Dictionary<string, string>
+            {
+                {"Laptop", "E100"},
+                {"Smartphone", "E101"},
+                {"Tablet", "E102"},
+                {"Headset", "E103"},
+                {"Keyboard", "E104"},
+                {"Mouse", "E105"},
+                {"Printer", "E106"},
+                {"Monitor", "E107"},
+                {"Smartwatch", "E108"},
+                {"Kamera", "E109"},
+            };
+
+            public string getKodeProduk ( string produk)
+            {
+            if (Produk.ContainsKey(produk))
+            {
+                return Produk[produk];
+            }
+                 return null;
+            }
+          }
+     }
+   
