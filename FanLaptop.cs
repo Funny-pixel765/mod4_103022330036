b01afe0daad0e230a9571ae8dfb27f8ed65f2a05
Funny-pixel765 <funnyrnd92@gmail.com>
2025-03-12 17:19:53 +0700

    menambahkan CLASS FANLAPTOP

---
+++ FanLaptop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod4_103022330036
+{
+    internal class FanLaptop
+    {
+        public enum FanState { Quiet, Balanced, Performance, Turbo };
+        private FanState currentState;
+        public FanLaptop()
+        {
+            currentState = FanState.Quiet;
+        }
+        public void Turbo()
+        {
+            currentState == FanState.Turbo)
+            {
+                currentState = FanState.Turbo;
+                Console.WriteLine("Fan Quiet berubah menjadi Turbo");
+            }
+        }
+        public void Quite()
+        {
+            if (currentState == FanState.Quiet)
+            {
+                currentState = FanState.Quiet;
+                Console.WriteLine("Fan Quiet berubah menjadi Quite");
+            }
+        }
+        public void Balanced()
+        {
+            if (currentState == FanState.Balanced)
+            {
+                currentState = FanState.Balanced;
+                Console.WriteLine("Fan Quiet berubah menjadi Balanced");
+            }
+        }
+        public void Performance()
+        {
+            if (currentState == FanState.Performance)
+            {
+                currentState = FanState.Performance;
+                Console.WriteLine("Fan Quiet berubah menjadi Performance");
+            }
+
+             public void ShowState()
+        {
+            Console.WriteLine($"Status fan saat ini: {currentState}");
+        }
+    
+    }
+
+}
+       
--- Program.cs
+++ Program.cs
@@ -4,17 +4,44 @@ using mod4_103022330036;
 internal class Program
 {
     private static void Main(string[] args)
-{
-    kodeProduk produk = new kodeProduk();
-    Console.WriteLine("Laptop " +produk.getKodeProduk("Laptop"));
-    Console.WriteLine("Smartphone " +produk.getKodeProduk("Smartphone"));
-    Console.WriteLine("Tablet " +produk.getKodeProduk("Tablet"));
-    Console.WriteLine("Headset " +produk.getKodeProduk("Headset"));
-    Console.WriteLine("Keyboard " +produk.getKodeProduk("Keyboard"));
-    Console.WriteLine("Mouse  "+produk.getKodeProduk("Mouse"));
-    Console.WriteLine("Printer "+ produk.getKodeProduk("Printer"));
-    Console.WriteLine("Monitor "+ produk.getKodeProduk("Monitor"));
+    {
+        kodeProduk produk = new kodeProduk();
+        Console.WriteLine("Laptop " + produk.getKodeProduk("Laptop"));
+        Console.WriteLine("Smartphone " + produk.getKodeProduk("Smartphone"));
+        Console.WriteLine("Tablet " + produk.getKodeProduk("Tablet"));
+        Console.WriteLine("Headset " + produk.getKodeProduk("Headset"));
+        Console.WriteLine("Keyboard " + produk.getKodeProduk("Keyboard"));
+        Console.WriteLine("Mouse  " + produk.getKodeProduk("Mouse"));
+        Console.WriteLine("Printer " + produk.getKodeProduk("Printer"));
+        Console.WriteLine("Monitor " + produk.getKodeProduk("Monitor"));
         Console.WriteLine("Smartwatch " + produk.getKodeProduk("Smartwatch"));
         Console.WriteLine("Kamera " + produk.getKodeProduk("Kamera"));
-}
-}
+
+        Console.WriteLine("\n");
+
+        FanLaptop fan = new FanLaptop();
+        Console.WriteLine("== Fan Laptop Status ==");
+        Console.WriteLine("== Status Awal==");
+        string choice = Console.ReadLine();
+        string command = Console.ReadLine();
+        switch (command)
+        {
+            case "Turbo":
+                fan.Turbo();
+                break;
+            case "Quite":
+                fan.Quite();
+                break;
+            case "Performance":
+                fan.Performance();
+                break;
+            case "Balanced":
+                fan.Balanced();
+                break;
+            default:
+                Console.WriteLine("Perintah tidak valid Silahkan coba lagi");
+                break;
+        }
+        fan.showState();
+    }
+    }
